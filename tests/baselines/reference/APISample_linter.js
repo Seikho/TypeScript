65a7e36000b3464b91fb74c1c2c24452e699301d
yortus <yortus@gmail.com>
2015-09-08 14:30:41 +0800

    update baseline (SyntaxKind numbering changed)

--- tests/baselines/reference/APISample_linter.js
+++ tests/baselines/reference/APISample_linter.js
@@ -75,28 +75,28 @@ function delint(sourceFile) {
     delintNode(sourceFile);
     function delintNode(node) {
         switch (node.kind) {
-            case 197 /* ForStatement */:
-            case 198 /* ForInStatement */:
-            case 196 /* WhileStatement */:
-            case 195 /* DoStatement */:
-                if (node.statement.kind !== 190 /* Block */) {
+            case 200 /* ForStatement */:
+            case 201 /* ForInStatement */:
+            case 199 /* WhileStatement */:
+            case 198 /* DoStatement */:
+                if (node.statement.kind !== 193 /* Block */) {
                     report(node, "A looping statement's contents should be wrapped in a block body.");
                 }
                 break;
-            case 194 /* IfStatement */:
+            case 197 /* IfStatement */:
                 var ifStatement = node;
-                if (ifStatement.thenStatement.kind !== 190 /* Block */) {
+                if (ifStatement.thenStatement.kind !== 193 /* Block */) {
                     report(ifStatement.thenStatement, "An if statement's contents should be wrapped in a block body.");
                 }
                 if (ifStatement.elseStatement &&
-                    ifStatement.elseStatement.kind !== 190 /* Block */ &&
-                    ifStatement.elseStatement.kind !== 194 /* IfStatement */) {
+                    ifStatement.elseStatement.kind !== 193 /* Block */ &&
+                    ifStatement.elseStatement.kind !== 197 /* IfStatement */) {
                     report(ifStatement.elseStatement, "An else statement's contents should be wrapped in a block body.");
                 }
                 break;
-            case 179 /* BinaryExpression */:
+            case 182 /* BinaryExpression */:
                 var op = node.operatorToken.kind;
-                if (op === 30 /* EqualsEqualsToken */ || op == 31 /* ExclamationEqualsToken */) {
+                if (op === 33 /* EqualsEqualsToken */ || op == 34 /* ExclamationEqualsToken */) {
                     report(node, "Use '===' and '!=='.");
                 }
                 break;
